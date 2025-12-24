52fdf271cc848d6aa658cecbe4d6c4044afe8760
slavabeez <slava_berdugin119@mail.ru>
2025-12-24 10:27:34 +0300

    Add files via upload

---
+++ работа 7/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath> 
+#include <numeric>
+#include <numbers>
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+
+struct working {
+public:
+    static double triangleArea(double& a, double& h) {
+        double S;
+        if (a <= 0 || h <= 0) {
+            throw invalid_argument("Основание и высота должны быть положительными");
+        }
+        S = 2 / (a * h);
+        cout << "площадь триугольника = " << S << endl;
+        return S;
+    }
+    static double rectangleArea(double& a, double& b) {
+        double S;
+        if (a <= 0 || b <= 0) {
+            throw invalid_argument("Длина и ширина должны быть положительными");
+        }
+        S = a * b;
+        cout << "площадь прямоугольника = " << S << endl;
+        return S;
+    }
+    static double circleArea(double& r) {
+        double S;
+        if (r <= 0) {
+            throw invalid_argument("Радиус должен быть положительным");
+        }
+        S = pow(r, 2) * PI;
+        cout << "площадь круга = " << S << endl;
+        return S;
+    }
+    static double trapezoidArea(double& a, double& b, double& h) {
+        double S;
+        if (a <= 0 || b <= 0 || h <= 0) {
+            throw invalid_argument("Основания и высота должны быть положительными");
+        }
+        S = ((a + b) * h) / 2;
+        cout << "площадь трапеции = " << S << endl;
+        return S;
+    }
+};
+
+int main() {
+    setlocale(0, "");
+    working праркеар;
+    int R;
+    double a = 1, b = 1, h = 1;
+    while (true)
+    {
+        cout << "1 площадь триугольника " << endl <<
+            "2 площадь прямоугольника " << endl <<
+            "3 площадь круга " << endl <<
+            "4 площадь трапеции " << endl <<
+            "5 выход " << endl;
+        cout << "выберите вариант действия: ";
+        cin >> R;
+        try
+        {
+            switch (R) {
+            case 1:
+                cout << "введите число a,h ";
+                cin >> a >> h;
+                праркеар.triangleArea(a, h);
+                break;
+            case 2:
+                cout << "введите число a,b ";
+                cin >> a >> b;
+                праркеар.rectangleArea(a, b);
+                break;
+            case 3:
+                cout << "введите число a ";
+                cin >> a;
+                праркеар.circleArea(a);
+                break;
+            case 4:
+                cout << "введите число a,b,h ";
+                cin >> a >> b >> h;
+                праркеар.trapezoidArea(a, b, h);
+                break;
+            case 5:
+                cout << "Завершение работы." << endl;
+                return 0;
+            default:
+                cout << "не верное число" << endl << endl;
+            }
+        }
+        catch (const invalid_argument& e) {
+            cout << "Ошибка: " << e.what() << endl;
+        }
+
+    }
+}
---
+++ работа 7/ConsoleApplication1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,118 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <stdexcept>
+#include "H:\Бердюгин Вячеслав\практика\работа 7\ConsoleApplication1\ConsoleApplication1\ConsoleApplication1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GeometryTests
+{
+    TEST_CLASS(GeometryTests)
+    {
+    public:
+
+        TEST_METHOD(TestTriangleArea_ValidInput_ReturnsCorrectArea)
+        {
+            // Arrange
+            working calculator;
+            double a = 10.0;
+            double h = 5.0;
+
+            // Act
+            double result = calculator.triangleArea(a, h);
+
+            // Assert - ВАША формула: 2/(a*h) = 2/(10*5) = 2/50 = 0.04
+            Assert::AreEqual(0.04, result, 0.001);
+        }
+
+        TEST_METHOD(TestRectangleArea_ValidInput_ReturnsCorrectArea)
+        {
+            // Arrange
+            working calculator;
+            double length = 4.0;
+            double width = 5.0;
+
+            // Act
+            double result = calculator.rectangleArea(length, width);
+
+            // Assert
+            Assert::AreEqual(20.0, result, 0.001);
+        }
+
+        TEST_METHOD(TestCircleArea_ValidRadius_ReturnsCorrectArea)
+        {
+            // Arrange
+            working calculator;
+            double radius = 3.0;
+
+            // Act
+            double result = calculator.circleArea(radius);
+
+            // Assert - π * 3² = 3.14159265 * 9 = 28.274333
+            Assert::AreEqual(28.274333, result, 0.001);
+        }
+
+        TEST_METHOD(TestTriangleArea_NegativeBase_ThrowsException)
+        {
+            // Arrange
+            working calculator;
+            double negativeBase = -5.0;
+            double height = 4.0;
+
+            auto func = [&calculator, &negativeBase, &height]() {
+                calculator.triangleArea(negativeBase, height);
+                };
+
+            // Act & Assert
+            Assert::ExpectException<std::invalid_argument>(func);
+        }
+
+        TEST_METHOD(TestRectangleArea_ZeroWidth_ThrowsException)
+        {
+            // Arrange
+            working calculator;
+            double length = 5.0;
+            double zeroWidth = 0.0;
+
+            auto func = [&calculator, &length, &zeroWidth]() {
+                calculator.rectangleArea(length, zeroWidth);
+                };
+
+            // Act & Assert
+            Assert::ExpectException<std::invalid_argument>(func);
+        }
+
+        // Дополнительные тесты для покрытия всех методов
+
+        TEST_METHOD(TestTrapezoidArea_ValidInput_ReturnsCorrectArea)
+        {
+            // Arrange
+            working calculator;
+            double a = 3.0;
+            double b = 5.0;
+            double h = 4.0;
+
+            // Act
+            double result = calculator.trapezoidArea(a, b, h);
+
+            // Assert - ((3+5)*4)/2 = (8*4)/2 = 32/2 = 16
+            Assert::AreEqual(16.0, result, 0.001);
+        }
+
+        TEST_METHOD(TestTrapezoidArea_NegativeHeight_ThrowsException)
+        {
+            // Arrange
+            working calculator;
+            double base1 = 3.0;
+            double base2 = 5.0;
+            double negativeHeight = -4.0;
+
+            auto func = [&calculator, &base1, &base2, &negativeHeight]() {
+                calculator.trapezoidArea(base1, base2, negativeHeight);
+                };
+
+            // Act & Assert
+            Assert::ExpectException<std::invalid_argument>(func);
+        }
+    };
+}
